cd36616465d20583d83e5da47383f045bb108f80
rymsk-21 <seki.r621@gmail.com>
2022-02-12 10:31:11 +0900

    Finish user sighup

---
+++ sample/test/integration/users_signup_test.rb
@@ -0,0 +1,21 @@
+module UsersHelper
+  # 引数で与えられたユーザーのGravatar 画像を す
+  def gravatar_for(user, size: 80)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Example User",
+                                         email: "user@example.com",
+                                         password: "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.  コードを書き む 
+  end
+end
