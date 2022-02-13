37c735819c3df2edeb173934e42a5812dad03a62
rymsk-21 <seki.r621@gmail.com>
2022-02-13 11:12:41 +0900

    feat/add_2022-02-13

--- sample/test/integration/users_signup_test.rb
+++ sample/test/integration/users_signup_test.rb
@@ -1,21 +1,13 @@
-module UsersHelper
-  # 引数で与えられたユーザーのGravatar 画像を す
-  def gravatar_for(user, size: 80)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
-  end
-
-  test "valid signup information" do
+require 'test_helper'
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
     get signup_path
-    assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name: "Example User",
-                                         email: "user@example.com",
-                                         password: "password",
-                                         password_confirmation: "password" } }
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: "",
+                                         email: "user@invalid",
+                                         password: "foo",
+                                         password_confirmation: "bar" } }
     end
-    follow_redirect!
-    assert_template 'users/show'
-    assert_not flash.  コードを書き む 
+    assert_template 'users/new'
   end
 end
