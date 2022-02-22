e91676030e0b3c900ec32ccecc3da652be651019
rymsk-21 <seki.r621@gmail.com>
2022-02-23 03:35:31 +0900

    Add account activation

--- sample/test/integration/users_signup_test.rb
+++ sample/test/integration/users_signup_test.rb
@@ -1,15 +1,20 @@
+
 require 'test_helper'
+
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  test "valid signup information" do
+
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
+  test "invalid signup information" do
     get signup_path
-    assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name: "Example User",
-                                         email: "user@example.com",
-                                         password: "password",
-                                         password_confirmation: "password" } }
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
     end
-    follow_redirect!
-    assert_template 'users/show'
-    assert is_logged_in?
+    assert_template 'users/new'
   end
 end
