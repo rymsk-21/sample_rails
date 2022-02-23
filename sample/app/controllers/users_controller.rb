ea8e444bfa5ce2e7c3d19b35c9fcce90b697506a
rymsk-21 <seki.r621@gmail.com>
2022-02-23 23:21:17 +0900

    13.3.2

--- sample/app/controllers/users_controller.rb
+++ sample/app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -51,25 +52,16 @@ class UsersController < ApplicationController
    def user_params
      params .require(:user).permit(:name, :email, :password, :password_confirmation)
    end
-
-  def admin_user
-    redirect_to(root_url) unless current_user.admin?
-  end
-
   #before アクション
-
-  # ログイン済みユーザかどうか確認
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
 
   # 正しいユーザかどうか確認
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
+  end
+
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
